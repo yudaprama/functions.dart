13b4250377badcc64b94e80ebd7c94460b0e5f75
[TD-Engineering] Yuda Prama <yudhaprama@icloud.com>
2020-05-04 00:07:52 +0700

    navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void horizontalNavigation(BuildContext context, Widget screen, [bool rootNavigator = false]) {
+	if (Platform.isIOS)
+		Navigator.of(context, rootNavigator: rootNavigator).push(
+			MaterialPageRoute(
+				builder: (BuildContext context) => screen,
+			),
+		);
+	else
+		Navigator.of(context, rootNavigator: rootNavigator).push(
+			CupertinoPageRoute(
+				builder: (BuildContext context) => screen,
+			),
+		);
+}
